ada22a67d1af166d8eb5aa1f58b12aa3b9038321
David Garner <dgarner@qmetric.co.uk>
2020-06-26 15:51:11 +0100

    Finish slides; add README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Deno Slides
+
+Slides for a talk I gave to my team on Deno.
+
+This project uses [mdx-deck][mdx-deck] for the slides, which are specified in [MDX][mdx] syntax [here][slides].
+
+[mdx]: https://mdxjs.com/
+[mdx-deck]: https://github.com/jxnblk/mdx-deck
+[slides]: ./slides.mdx
+
+To run this project:
+
+```
+yarn && yarn start
+```
+
+To turn on speaker notes: `Option + P`.
+
+## Links
+
+- Deno: https://deno.land/
+- Deno Manual: https://deno.land/manual
+- 10 Things I regret about Node.js: https://www.youtube.com/watch?v=M3BM9TB-8yA
---
+++ theme.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Highlight, { defaultProps } from "prism-react-renderer";
+import codeTheme from "prism-react-renderer/themes/vsDark";
+import * as themes from '@mdx-deck/themes'
+
+// TODO fix to not just be JSX-only
+// Yes this is hacky as hell but I was rushed ¯\_(ツ)_/¯
+const CodeBlock = ({ children }) => {
+  return (
+    <Highlight {...defaultProps} theme={codeTheme} code={children} language="javascript">
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: "20px 20px 0" }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
+
+const theme= {
+  ...themes.dark,
+  colors: {
+    background: "#111",
+    text: "#fff",
+  },
+  styles: {
+    a: {
+      color: '#73dbeb',
+    },
+    li: {
+      marginTop: 20,
+      marginBottom: 20,
+    },
+    blockquote: {
+      background: '#333',
+      padding: '0 30px',
+    }
+  },
+  components: {
+    code: CodeBlock,
+  }
+}
+
+// console.log(theme);
+
+export const Attribution = ({ children }) => <div style={{ position: 'absolute', bottom: 0, right: 20, fontSize: 20 }}>{children}</div>
+
+export default theme;
